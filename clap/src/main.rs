b0ebdb75cbbe86b310ed915a0844835d996943d7
James Devine <t-jamdev@microsoft.com>
2022-06-14 13:50:21 +0100

    initial draft of repro

---
+++ clap/src/main.rs
@@ -0,0 +1,47 @@
+use clap::{App, Arg};
+use std::fmt;
+use std::fs;
+use std::collections::HashMap;
+use yaml_rust::{YamlLoader, Yaml};
+
+pub struct CLIApplication {
+    name: String,
+    cli: App<'static, 'static>,
+}
+
+impl CLIApplication {
+}
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+pub fn context(yaml_path: &str) -> App<'static, 'static> {
+    let command_configuration: String = match fs::read_to_string(yaml_path) {
+        Ok(s) => String::from(s),
+        Err(e) => panic!("unable to open configuration file: {}", e),
+    };  
+    
+    let yml_docs = match YamlLoader::load_from_str(&command_configuration) {
+        Ok(yml) => yml,
+        Err(e)=> panic!("Unable to parse yaml configuration: {}", e),
+    };
+
+    print_type_of(&yml_docs);
+    let document = &yml_docs[0];
+    print_type_of(&document);
+    let app_name = document["name"].as_str().unwrap();
+    println!("{:?}",document);
+
+    // return from_new("app");
+
+    return CLIApplication {
+        name: String::from(yaml_path),
+        cli: App::from_yaml(&yml_docs[0]),
+    }
+}
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
